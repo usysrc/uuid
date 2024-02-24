1aa7c897ce93a422b619dfec796cd0a390406ced
usysrc <usysrc@usysrc.dev>
2024-02-24 21:22:54 +0100

    style: :art: formatting

--- main.go
+++ main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-  "fmt"
-  "github.com/google/uuid"
-  "flag"
+	"flag"
+	"fmt"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-  
-  count := flag.Int("n", 1, "number of UUIDs to generate")
-  verify := flag.String("v", "", "verify a UUID")
 
-  flag.Parse()
+	count := flag.Int("n", 1, "number of UUIDs to generate")
+	verify := flag.String("v", "", "verify a UUID")
 
-  if *verify != "" {
-    _, err := uuid.Parse(*verify)
-    if err != nil {
-      fmt.Println("invalid")
-      return
-    }
-    fmt.Println("valid")
-    return
-  }
+	flag.Parse()
 
-  for i:=0; i < *count; i++ {
-    fmt.Println(uuid.NewString());
-  }
+	if *verify != "" {
+		_, err := uuid.Parse(*verify)
+		if err != nil {
+			fmt.Println("invalid")
+			return
+		}
+		fmt.Println("valid")
+		return
+	}
+
+	for i := 0; i < *count; i++ {
+		fmt.Println(uuid.NewString())
+	}
 }
